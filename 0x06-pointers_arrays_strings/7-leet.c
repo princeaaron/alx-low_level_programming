f03bd945e8b259c88adb3dec3c6ccc8dff0f5782
princeaaron <117784650+princeaaron@users.noreply.github.com>
2022-12-22 22:50:45 +0000

    pointers_arrays_strings_2

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *leet - encodes a string into 1337.
+ *@s: string to be crypted .
+ *Return: pointer to string
+ */
+
+char *leet(char *s)
+{
+	char *m = "aeotl";
+	char *M = "AEOTL";
+	int x[] = {'4', '3', '0', '7', '1'};
+	int i;
+	char *p = s;
+
+	while (*s != '\0')
+	{
+
+	for (i = 0; i < 5; i++)
+	{
+		if (*s == *(m + i) || *s == *(M + i))
+
+			*s = x[i];
+	}
+		s++;
+
+	}
+		return (p);
+}
