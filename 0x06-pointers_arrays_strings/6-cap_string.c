17ec85fdc8126b97b39adb0a75845a623c4937bd
princeaaron <117784650+princeaaron@users.noreply.github.com>
2022-12-22 23:13:53 +0000

    pointers_arrays_strings_2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,18 +8,19 @@
 
 char *cap_string(char *str)
 {
-	int obj[14] = {' ', '\t', '\n', ',', ';', '.', '!',
+	int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!',
 		'?', '"', '(', ')', '{', '}'};
-	int x, y;
+	int i, j;
 
-	for (x = 0; str[x] != '\0'; x++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[0] >= 97 && str[0] <= 122)
 	{
 			str[0] = str[0] - 32;
 	}
-	for (y = 0; y < 14; y++)
-		if (str[x] >= 97 && str[x] <= 122 && str[x - 1] == obj[y])
-			str[x] = str[x] - 32;
+	for (j = 0; j < 14; j++)
+		if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[j])
+			str[i] = str[i] - 32;
 	}
 	return (str);
+}
