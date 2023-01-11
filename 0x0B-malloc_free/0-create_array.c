f59497b3a2267c84852786d8fdde590cc329f316
princeaaron <117784650+princeaaron@users.noreply.github.com>
2023-01-11 00:12:19 +0000

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,31 +1,33 @@
 #include "main.h"
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * create_array - an array of chars
- * and initializes it with a specific char.
- * @size : size of an array.
- * @c: character.
- * Return: pointer to the Array or NULL if it fails.
+ * create_array - creates an array of chars,
+ * and initializes it with a char.
+ * @size: size of array
+ * @c: specific character to initialize the array with.
+ *
+ * Return: pointer to the base address of the array.
  */
 char *create_array(unsigned int size, char c)
 {
-char *arrPointer;
-unsigned int x;
+	char *arrPtr;
+	unsigned int x;
 
-if (size == 0)
-	return (NULL);
-arrPointer = malloc(sizeof(char) * size);
-/*
- * Allocating a specific nb of bytes on the memory
- * and returning a pointer to the allocated memory
- */
-if (arrPointer == NULL)
-	return (NULL);
-else
-{
+	if (size == 0)
+		return (NULL);
+
+	arrPtr = (char *)malloc(sizeof(char) * size);
+
+	if (arrPtr == NULL)
+		return (NULL);
+
 	for (x = 0; x < size; x++)
-		arrPointer[x] = c;
-		return (arrPointer);
-}
+	{
+		arrPtr[x] = c;
+	}
+
+	arrPtr[++x] = '\0';
+	return (arrPtr);
 }
