10ab382aa3e18cf74fe1eef27198ff9e1eb07ffa
princeaaron <117784650+princeaaron@users.noreply.github.com>
2023-01-11 00:21:41 +0000

    malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,6 @@
  * @s2: string 2
  * Return: pointer to new string, NULL if it fails
  */
-
 char *str_concat(char *s1, char *s2)
 {
     int len1, len2, len3;
@@ -18,26 +17,21 @@ char *str_concat(char *s1, char *s2)
         s1 = "";
     if (s2 == NULL)
         s2 = "";
-
+    
     len1 = 0;
     while (*(s1 + len1) != '\0')
     {
         len1++;
     }
-
     len2 = 0;
     while (*(s2 + len2) != '\0')
     {
         len2++;
     }
-
     len3 = len1 + len2;
-
     arrChar = (char*) malloc(len3 * sizeof(char) + 1);
-
     if (arrChar == NULL)
         return (NULL);
-
     for (i = 0; i < len1; i++)
         arrChar[i] = s1[i];
     for (i = 0; i < len2; i++)
