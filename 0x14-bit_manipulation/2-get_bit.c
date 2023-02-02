fe443efb4ddb71260ed6b9c4249e92e1a8e4fb0f
princeaaron <117784650+princeaaron@users.noreply.github.com>
2023-02-02 22:58:05 +0000

    bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * get_bit - Entry Point
- * @n: input
+ * get_bit - returns the value of a bit at a given index
+ * @n: integer input
  * @index: index
- * Return: 0
+ * Return: integer
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
